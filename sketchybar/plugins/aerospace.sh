a316ed6433ac603f175952b2c06f0e87d9dbdf85
samkaj <kajavasamuel@gmail.com>
2024-11-01 10:46:01 +0100

    add vs code icon for codeql stuff and change active label color

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME label.color=0xffffae9f
+    sketchybar --set $NAME label.color=0xffecaad6
 else
     sketchybar --set $NAME label.color=0xffffffff
 fi
